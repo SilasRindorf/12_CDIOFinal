0beba14386ec6abf5c74c88a6656a21789a86859
Silas Rindorf <s195474@student.dtu.dk>
2020-06-12 12:56:05 +0200

    API

--- src/main/java/api/TestService.java
+++ src/main/java/api/TestService.java
@@ -1,4 +1,4 @@
-package API;
+package api;
 
 /***
  * Initial version created by: Silas
---
+++ src/main/java/controllers/ActionController.java
@@ -0,0 +1,7 @@
+package controllers;
+
+public class ActionController {
+    public void logIn(String uname, String password){
+        System.out.println("Logging in:\nusername="  + uname + "\npassword=" + password);
+    }
+}
